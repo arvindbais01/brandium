a4ac4de9d92930f5431e5a4f237c0b07eb376430
arvindbais01 <arvindbais367@gmai.com>
2024-03-19 22:49:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function locomotiveAnimation() {
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+
+    tablet: { smooth: true },
+
+    smartphone: { smooth: true }
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight
+      };
+    }
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+
+
+
+
+}
+
+locomotiveAnimation();
+gsap.from("#page1 h1", {
+  y: 600,
+  duration: 1,
+  delay: 0.5,
+  stagger: 0.2,
+})
